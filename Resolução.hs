502200b92fe3c54de3e68decdf8c7953c895584d
Rodrigo Reis <N-genhocas@hotmail.com>
2016-09-22 15:49:31 +0100

    Adição da resolução do Ex. 6 - 10

--- Resolução.hs
+++ Resolução.hs
@@ -20,7 +20,7 @@ diferencaTres x y z = abs (x - y) > z
 addDigit x y = if x >= 0 
 		then (x * 10) + y
 		else (x * 10) - y
---OU
+-- OU
 addDigit' x y = (x * 10) + if x >= 0 then y else (-y)
 
 -- Exercicio 4
@@ -50,7 +50,7 @@ tiraPrimeiroUltimo x = tail(init x)
 -- d)
 
 segundoElemento x = x!!1
---OU
+-- OU
 segundoElemento' x = head(tail x)
 
 -- e)
@@ -60,7 +60,7 @@ penultimoElemento x = last (init x)
 -- f)
 
 invetExceptUltimo xs = head xs : reverse (tail xs)
--- ou
+-- OU
 inverteExceptoUltimo (x:xs) = x : (reverse xs)
 
 -- g)
@@ -70,3 +70,28 @@ somaPrimeiros5 x = sum (take 5 x)
 -- h)
 
 somaNPrimeiros n x = sum (take n x)
+
+-- Exercicio 6
+
+partition s e p = [ x | x <- [s,s + ((e-s)/p)..e]]
+
+-- Exercicio 7
+
+-- a) [2,4,6]
+-- b) [4,16,36,64]
+-- c) "6789"
+-- d) [(1,1),(1,2),(1,3),(3,1),(3,2),(3,3)]
+-- e) [(1,1),(1,2),(1,3),(3,1),(3,2),(3,3)]
+
+-- Exercicio 8
+
+soma100Quadrados = sum [ x^2 | x <- [1..100]]
+
+-- Exercicio 9
+
+triploPitagorico n = [(x,y,z) | x <- [1..n], y <- [1..n], z <- [1..n], (x^2 + y^2 == z^2)]
+
+-- Exercicio 10
+
+factors n = [x | x <- [1..(n-1)], (mod n x) == 0]
+perfects n = [x | x <- [1..n], x == sum(factors x)]
